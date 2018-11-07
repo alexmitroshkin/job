e3aaddc9eb51dd9b3cf4a3b750cf11de7363f6d5
Alex Mitroshkin <mitroshkin.a@yandex.ru>
2018-11-07 20:10:46 +0700

    delete book2

--- src/java/ru/mitroshkinam/journalLpv/service/MySqlStorage.java
+++ src/java/ru/mitroshkinam/journalLpv/service/MySqlStorage.java
@@ -5,9 +5,14 @@ import java.sql.Connection;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import javax.annotation.Resource;
+import javax.sql.DataSource;
 
 public class MySqlStorage {
     private Connection connection;
+    @Resource(lookup = "java:/comp/env/jdbc/journallpv")
+//    @Resource(name = "jdbc/journallpv")
+    private DataSource dataSource;
 
     public MySqlStorage() {
         try {
